a6a317a7e57914ab2717842b8e9cd1e282121d0a
encodeous <encodeous7@gmail.com>
2020-12-22 13:26:25 -0500

    Infnite v1

---
+++ Controllers/InfniteFile.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace infnite.Controllers
+{
+    [ApiController]
+    public class Infinity : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Get()
+        {
+            return RedirectPermanent("/swagger/");
+        }
+    }
+}
